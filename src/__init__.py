2ee2fb2146303e30d8e1249977d30955f15ecddf
Myles Ward <mw@yellowdave.co.uk>
2025-10-01 14:04:25 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
+# making SRC a Python package
 
+from .var_methods import historical_var, parametric_var, monte_carlo_var
+from .es_methods import historical_es
+from .portfolio import simulate_portfolio_paths
+from .backtesting import kupiec_test
