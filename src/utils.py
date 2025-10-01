774924c160eb8687ed0b81bf81e88a3692a9bded
Myles Ward <mw@yellowdave.co.uk>
2025-10-01 14:05:15 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
+# just helper functions
 
+import matplotlib.pyplot as plt
+
+def plot_var_distribution(returns, var_value, alpha=0.05):
+    plt.hist(returns, bins=50, alpha=0.7)
+    plt.axvline(-var_value, color="red", linestyle="--", label=f"VaR {int((1-alpha)*100)}%")
+    plt.legend()
+    plt.show()
