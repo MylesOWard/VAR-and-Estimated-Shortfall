7fc4593684afd83db8f55921c5a4d958ee91ce34
Myles Ward <mw@yellowdave.co.uk>
2025-10-01 14:02:51 +0100

    Update portfolio.py

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,1 +1,12 @@
+# tools for portfoilio simulation, specifically using MC (multiple paths / simulations)
 
+import numpy as np
+
+def simulate_portfolio_paths(returns, weights, horizon=10, sims=10000, seed=None):
+    if seed:
+        np.random.seed(seed)
+    mu = returns.mean().values
+    cov = returns.cov().values
+    paths = np.random.multivariate_normal(mu, cov, size=(sims, horizon))
+    portfolio_paths = paths @ weights
+    return portfolio_paths
